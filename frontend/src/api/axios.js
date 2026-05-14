527cdd8a87ab84f542170b6bfc693c5c17931311
hopvk6a81 <tungtran170806@gmail.com>
2026-05-15 01:47:31 +0700

    Add CorsConfig and Dockerfile for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,4 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' })
-export default api
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || '/api'
+});
+
+export default api;
